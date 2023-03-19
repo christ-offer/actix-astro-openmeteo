50c35c05c85891e7b742840518accb3974fd2777
Chris Holst <chris@seam.com>
2023-03-19 11:46:48 +0100

    slight refactor, moved the impl out of the function to clean the file up a bit

--- src/api/openmeteo.rs
+++ src/api/openmeteo.rs
@@ -1,6 +1,6 @@
-use actix_web::{post, web, HttpResponse, Responder, Result};
+use actix_web::{post, web, HttpResponse, Responder, Result, error::ResponseError, http::StatusCode};
 use reqwest::Client;
-//use std::cmp::Ordering;
+use std::fmt::{self, Display, Formatter};
 
 // get a model from ../models/mod.rs
 use crate::models::openmeteo::Daily;
@@ -12,12 +12,136 @@ use crate::models::openmeteo::StatMap;
 use crate::models::openmeteo::Stats;
 use crate::models::osm::Direction;
 
-#[post("/api/openmeteo")]
-async fn openmeteo(open_meteo_request: web::Json<OpenMeteoRequest>) -> Result<impl Responder> {
-    const USER_AGENT: &str = "User-Agent";
-    const CONTENT_TYPE: &str = "Content-Type";
-    const ACCEPT: &str = "Accept";
+const USER_AGENT: &str = "User-Agent";
+const CONTENT_TYPE: &str = "Content-Type";
+const ACCEPT: &str = "Accept";
 
+// Customer error handling for reqwest so the ? traits work properly inside the openmeteo function
+#[derive(Debug)]
+pub enum CustomError {
+    ReqwestError(reqwest::Error),
+    // You can add more error types if needed
+}
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            CustomError::ReqwestError(e) => write!(f, "ReqwestError: {}", e),
+        }
+    }
+}
+
+impl ResponseError for CustomError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            CustomError::ReqwestError(_) => HttpResponse::new(StatusCode::BAD_GATEWAY),
+        }
+    }
+}
+
+impl From<reqwest::Error> for CustomError {
+    fn from(error: reqwest::Error) -> Self {
+        CustomError::ReqwestError(error)
+    }
+}
+
+
+
+
+impl Daily {
+    pub fn calculate_stats(&self) -> Stats {
+        Stats {
+            temperature_2m_max: Self::calculate_stat_map(&self.temperature_2m_max, &self.time),
+            temperature_2m_min: Self::calculate_stat_map(&self.temperature_2m_min, &self.time),
+            temperature_2m_mean: Self::calculate_stat_map(
+                &self.temperature_2m_mean,
+                &self.time,
+            ),
+            apparent_temperature_max: Self::calculate_stat_map(
+                &self.apparent_temperature_max,
+                &self.time,
+            ),
+            apparent_temperature_min: Self::calculate_stat_map(
+                &self.apparent_temperature_min,
+                &self.time,
+            ),
+            apparent_temperature_mean: Self::calculate_stat_map(
+                &self.apparent_temperature_mean,
+                &self.time,
+            ),
+            shortwave_radiation_sum: Self::calculate_stat_map(
+                &self.shortwave_radiation_sum,
+                &self.time,
+            ),
+            precipitation_sum: Self::calculate_stat_map(&self.precipitation_sum, &self.time),
+            precipitation_hours: Self::calculate_stat_map(
+                &self.precipitation_hours,
+                &self.time,
+            ),
+            windspeed_10m_max: Self::calculate_stat_map(&self.windspeed_10m_max, &self.time),
+            windgusts_10m_max: Self::calculate_stat_map(&self.windgusts_10m_max, &self.time),
+            et0_fao_evapotranspiration: Self::calculate_stat_map(
+                &self.et0_fao_evapotranspiration,
+                &self.time,
+            ),
+            snowfall_sum: Self::calculate_stat_map(&self.snowfall_sum, &self.time),
+        }
+    }
+
+    fn calculate_stat_map(data: &Vec<Option<f64>>, times: &Vec<Option<String>>) -> StatMap {
+        let mut min = None;
+        let mut max = None;
+        let mut sum = 0.0;
+        let mut count = 0;
+
+        for (i, value) in data.iter().enumerate() {
+            if let Some(val) = value {
+                if let Some(_time) = times[i].as_ref() {
+                    count += 1;
+                    sum += val;
+
+                    match min {
+                        None => min = Some((i, *val)),
+                        Some((_, min_val)) if *val < min_val => min = Some((i, *val)),
+                        _ => (),
+                    }
+
+                    match max {
+                        None => max = Some((i, *val)),
+                        Some((_, max_val)) if *val > max_val => max = Some((i, *val)),
+                        _ => (),
+                    }
+                }
+            }
+        }
+
+        let mean = if count > 0 { sum / count as f64 } else { 0.0 };
+
+        StatMap {
+            min: MinMaxMean {
+                time: min
+                    .map(|(i, _)| times[i].clone().unwrap())
+                    .unwrap_or("".to_string()),
+                value: min.map(|(_, v)| v).unwrap_or(0.0),
+            },
+            max: MinMaxMean {
+                time: max
+                    .map(|(i, _)| times[i].clone().unwrap())
+                    .unwrap_or("".to_string()),
+                value: max.map(|(_, v)| v).unwrap_or(0.0),
+            },
+            mean: MinMaxMean {
+                time: "".to_string(),
+                value: mean,
+            },
+        }
+    }
+}
+
+
+
+#[post("/api/openmeteo")]
+async fn openmeteo(open_meteo_request: web::Json<OpenMeteoRequest>) -> Result<impl Responder, CustomError> {
     let city = &open_meteo_request.city;
 
     // Get the coordinates of the city from the OSM API
@@ -33,19 +157,15 @@ async fn openmeteo(open_meteo_request: web::Json<OpenMeteoRequest>) -> Result<im
         .header(CONTENT_TYPE, "application/json")
         .header(ACCEPT, "application/json")
         .send()
-        .await
-        .unwrap()
+        .await?
         .json::<Vec<Direction>>()
-        .await;
+        .await?;
 
-    let osm_data = response.unwrap();
-    let lat = osm_data[0].lat.to_string();
-    let lon = osm_data[0].lon.to_string();
+    let osm_data = response;
+    let lat = osm_data[0].lat.clone();
+    let lon = osm_data[0].lon.clone();
 
-    let rez = Direction {
-        lat: lat.to_string(),
-        lon: lon.to_string(),
-    };
+    let rez = Direction { lat, lon };
 
     let start_date = &open_meteo_request.start_date;
     let end_date = &open_meteo_request.end_date;
@@ -59,112 +179,14 @@ async fn openmeteo(open_meteo_request: web::Json<OpenMeteoRequest>) -> Result<im
         .header(CONTENT_TYPE, "application/json")
         .header(ACCEPT, "application/json")
         .send()
-        .await
-        .unwrap()
+        .await?
         .json::<OpenMeteo>()
-        .await;
+        .await?;
 
-    let daily_data = meteo_response.unwrap();
-
-    let daily_data_copy = daily_data.clone();
-
-    impl Daily {
-        pub fn calculate_stats(&self) -> Stats {
-            Stats {
-                temperature_2m_max: Self::calculate_stat_map(&self.temperature_2m_max, &self.time),
-                temperature_2m_min: Self::calculate_stat_map(&self.temperature_2m_min, &self.time),
-                temperature_2m_mean: Self::calculate_stat_map(
-                    &self.temperature_2m_mean,
-                    &self.time,
-                ),
-                apparent_temperature_max: Self::calculate_stat_map(
-                    &self.apparent_temperature_max,
-                    &self.time,
-                ),
-                apparent_temperature_min: Self::calculate_stat_map(
-                    &self.apparent_temperature_min,
-                    &self.time,
-                ),
-                apparent_temperature_mean: Self::calculate_stat_map(
-                    &self.apparent_temperature_mean,
-                    &self.time,
-                ),
-                shortwave_radiation_sum: Self::calculate_stat_map(
-                    &self.shortwave_radiation_sum,
-                    &self.time,
-                ),
-                precipitation_sum: Self::calculate_stat_map(&self.precipitation_sum, &self.time),
-                precipitation_hours: Self::calculate_stat_map(
-                    &self.precipitation_hours,
-                    &self.time,
-                ),
-                windspeed_10m_max: Self::calculate_stat_map(&self.windspeed_10m_max, &self.time),
-                windgusts_10m_max: Self::calculate_stat_map(&self.windgusts_10m_max, &self.time),
-                et0_fao_evapotranspiration: Self::calculate_stat_map(
-                    &self.et0_fao_evapotranspiration,
-                    &self.time,
-                ),
-                snowfall_sum: Self::calculate_stat_map(&self.snowfall_sum, &self.time),
-            }
-        }
-
-        fn calculate_stat_map(data: &Vec<Option<f64>>, times: &Vec<Option<String>>) -> StatMap {
-            let mut min = None;
-            let mut max = None;
-            let mut sum = 0.0;
-            let mut count = 0;
-
-            for (i, value) in data.iter().enumerate() {
-                if let Some(val) = value {
-                    if let Some(_time) = times[i].as_ref() {
-                        count += 1;
-                        sum += val;
-
-                        match min {
-                            None => min = Some((i, *val)),
-                            Some((_, min_val)) if *val < min_val => min = Some((i, *val)),
-                            _ => (),
-                        }
-
-                        match max {
-                            None => max = Some((i, *val)),
-                            Some((_, max_val)) if *val > max_val => max = Some((i, *val)),
-                            _ => (),
-                        }
-                    }
-                }
-            }
-
-            let mean = if count > 0 { sum / count as f64 } else { 0.0 };
-
-            StatMap {
-                min: MinMaxMean {
-                    time: min
-                        .map(|(i, _)| times[i].clone().unwrap())
-                        .unwrap_or("".to_string()),
-                    value: min.map(|(_, v)| v).unwrap_or(0.0),
-                },
-                max: MinMaxMean {
-                    time: max
-                        .map(|(i, _)| times[i].clone().unwrap())
-                        .unwrap_or("".to_string()),
-                    value: max.map(|(_, v)| v).unwrap_or(0.0),
-                },
-                mean: MinMaxMean {
-                    time: "".to_string(),
-                    value: mean,
-                },
-            }
-        }
-    }
-
+    let daily_data_copy = meteo_response.clone();
     let stats = daily_data_copy.daily.calculate_stats();
-
     let sorted_daily_data = daily_data_copy.daily.clone();
 
-    // Calculate the max temperature of the sorted_daily_data.temperature_2m_max vector and save the index
-    // push the max temperature to the return_open_meteo.daily.temperature_2m_max.max.value
-    // push the time of the max temperature to the return_open_meteo.daily.temperature_2m_max.max.time
 
     let return_open_meteo = ReturnOpenMeteo {
         latitude: daily_data_copy.latitude,
